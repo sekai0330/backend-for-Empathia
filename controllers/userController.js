f262347a28440b1d299f6219eef117a1aa135f10
sekai0330 <asamieto965@gmail.com>
2024-04-20 04:04:37 +0900

    fix : voice input APIs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,43 +1,43 @@
-const User = require('../models/userModel')
-const jwt = require('jsonwebtoken')
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 // creating jwt token
 const createToken = (_id) => {
-    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
-}
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // login user
 const loginUser = async (req, res) => {
-    const {email, password} = req.body
-    
-    // logging in user, try catch block for errors
-    try {
-        const user = await User.login(email, password)
+  const { email, password } = req.body;
 
-        // create a token, authenticating user right after signing up
-        const token = createToken(user._id)
+  // logging in user, try catch block for errors
+  try {
+    const user = await User.login(email, password);
 
-        res.status(200).json({email, token})
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-}
+    // create a token, authenticating user right after signing up
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 // signup user
 const signupUser = async (req, res) => {
-    const {email, password} = req.body
+  const { email, password } = req.body;
 
-    // signing up user, try catch block for errors
-    try {
-        const user = await User.signup(email, password)
+  // signing up user, try catch block for errors
+  try {
+    const user = await User.signup(email, password);
 
-        // create a token, authenticating user right after signing up
-        const token = createToken(user._id)
+    // create a token, authenticating user right after signing up
+    const token = createToken(user._id);
 
-        res.status(200).json({email, token})
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-}
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-module.exports = {signupUser, loginUser}
+module.exports = { signupUser, loginUser };
